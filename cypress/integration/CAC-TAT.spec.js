a71283b81acbeff0d81ef88a2780a6d4fbd3e21c
Jheimys Bassani <jheimysbb@gmail.com>
2024-05-23 12:22:29 +0100

    Studying the Hook beforeEach and the get command.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -8,8 +8,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
-    it('Verifica o titulo da aplicação', () => {
+    beforeEach(() => {
         cy.visit('./src/index.html')
+    })
+
+    it('Verifica o titulo da aplicação', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('James')
+        cy.get('#lastName').type('Bassani')
+        cy.get('#email').type('james_bassani@yahoo.com')
+        cy.get('#open-text-area').type('O Cypress é bem legal')
+        cy.get('.button').click()
+        cy.get('.success').should('be.visible', 'Mensagem enviada com sucesso.')
     })
 })
