0e0495b5ba150ffc1fcb208cb14be2c7079e00d3
Jheimys Bassani <jheimysbb@gmail.com>
2024-06-18 11:13:44 +0100

    Marking radio type input

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -114,10 +114,25 @@ describe('Central de Atendimento ao Cliente TAT', () => {
             .should('have.value', 'mentoria')
     })
 
-    it.only('seleciona um produto (Blog) por seu índice', () => {
+    it('seleciona um produto (Blog) por seu índice', () => {
         cy.get('#product')
             .select(1)
             .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', () => {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
+    })
+
+    it.only('marca cada tipo de atendimento', () => {
+         cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(($radio) => {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
     })
 
 })
